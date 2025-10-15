d5cbef02243002347cc56e75c294b4edc8bed455
LorenaLira05 <naheelee2005@gmail.com>
2025-10-15 12:07:37 -0300

    Create criar_pedido.sql

---
+++ criar_pedido.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+
+CREATE PROCEDURE criar_pedido (
+    IN p_usuario_id INT,
+    IN p_status_id INT,
+    IN p_data DATE,
+    IN p_veiculo VARCHAR(100)
+)
+BEGIN
+    -- Insere um novo pedido
+    INSERT INTO pedido (usuario_id, status_id, data_criacao)
+    VALUES (p_usuario_id, p_status_id, p_data);
+
+    -- Retorna o ID do pedido recém-criado
+    SELECT LAST_INSERT_ID() AS novo_pedido_id;
+END$$
+
+DELIMITER ;
