11e35a096f848dd4144f5abbaa138f3700b47fab
LorenaLira05 <naheelee2005@gmail.com>
2025-10-15 12:12:48 -0300

    Update registrar_entrega.sql

--- registrar_entrega.sql
+++ registrar_entrega.sql
@@ -6,6 +6,11 @@ CREATE PROCEDURE registrar_entrega (
     IN p_data DATE
 )
 BEGIN
+IF p_data > CURDATE() THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Erro: data de entrega não pode ser futura.';
+    END IF;
+    
     INSERT INTO entrega (item_id, data_entrega, status_id)
     VALUES (p_item_id, p_data, p_status_id);
 END$$
