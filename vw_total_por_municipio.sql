b8d42cce2d72c48f91ecb5767eaf445c614dd0e1
LorenaLira05 <naheelee2005@gmail.com>
2025-10-15 11:50:07 -0300

    Update vw_total_por_municipio.sql

--- vw_total_por_municipio.sql
+++ vw_total_por_municipio.sql
@@ -1,8 +1,11 @@
 CREATE OR REPLACE VIEW vw_total_por_municipio AS
-SELECT m.nome AS municipio,
-       SUM(it.quantidade) AS total_distribuido
+SELECT 
+    m.nome AS municipio,
+    SUM(it.quantidade) AS total_distribuido
 FROM entrega en
 JOIN item_pedido it ON it.id = en.item_id
 JOIN pedido pd ON pd.id = it.pedido_id
-JOIN municipio m ON m.id = pd.usuario_id  --Checar esta linha
+JOIN usuario u ON u.id = pd.usuario_id
+JOIN endereco e ON e.id = u.endereco_id
+JOIN municipio m ON m.id = e.municipio_id
 GROUP BY m.nome;
