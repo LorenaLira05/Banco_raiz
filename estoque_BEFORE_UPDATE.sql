3dbb35baadb979550a8bf5247a5b941ce48f6039
LorenaLira05 <naheelee2005@gmail.com>
2025-10-15 11:38:05 -0300

    Create estoque_BEFORE_UPDATE.sql

---
+++ estoque_BEFORE_UPDATE.sql
@@ -0,0 +1,7 @@
+CREATE DEFINER = CURRENT_USER TRIGGER `mydb`.`	estoque_BEFORE_UPDATE` BEFORE UPDATE ON `estoque` FOR EACH ROW
+BEGIN
+IF NEW.quantidade_disponivel < 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Erro: saldo de estoque não pode ser negativo';
+    END IF;
+END
