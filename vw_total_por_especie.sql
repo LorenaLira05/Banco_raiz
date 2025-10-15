017244403d4dda8cb8ea74b5bbc9ce1a402013d0
LorenaLira05 <naheelee2005@gmail.com>
2025-10-15 11:53:25 -0300

    Update vw_total_por_especie.sql

--- vw_total_por_especie.sql
+++ vw_total_por_especie.sql
@@ -1,8 +1,8 @@
-CREATE VIEW vw_total_por_especie AS
+CREATE OR REPLACE VIEW vw_total_por_especie AS
 SELECT e.nome_popular AS especie,
        SUM(it.quantidade) AS total_distribuido
 FROM item_pedido it
 JOIN pedido pd ON pd.id = it.pedido_id
-JOIN estoque es ON es.id = pd.id
+JOIN estoque es ON es.id = it.estoque_id
 JOIN especies e ON e.id = es.especie_id
 GROUP BY e.nome_popular;
